89989af4150154ff88c1df9ca2bd3ec0e37c84cf
Yumna Sajal <yumnasajal006@gmail.com>
2026-07-17 15:16:10 +0500

    Completed login and signup JS functionality task-2

--- Practice/Scripts/practice3.js
+++ Practice/Scripts/practice3.js
@@ -47,8 +47,137 @@ for (let li of list_li){
     li.classList.toggle('highlight');
 }
 
+// parent child sibiling elements
+// .parentElement .childElementCount .children/.children[0] .nextElementSibiling .previousElementSibiling
+const sq_img = document.querySelector('.square');
+console.log(sq_img.nextElementSibling);
+
 // creating and removing elements;
 // document.createElement('div')
 
+const new_img = document.createElement('img');
+new_img.src = 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png';
+new_img.alt = 'Google Logo';
+new_img.classList.add('square');
+document.body.appendChild(new_img);
+
+// .append is a bit more flexible, can be used to add text also
+// .prepend -> at the start
+// .insertAdjacentElement(position, inserting element);
+// .after() .before() .remove() .replaceWith()
+
+const heading2 = document.createElement('h2');
+heading2.append('Adorable Chickens');
+heading2.classList.add('purple');
+const heading1 = document.querySelectorAll('h1')[2];
+heading1.insertAdjacentElement('afterend', heading2);
+
+// remove elements
+// .removeChild()
+const first_li = document.querySelector('li');
+first_li.parentElement.removeChild(first_li);
+
+const second_li = document.querySelectorAll('li')[1];
+second_li.remove();
+
+//events
+//buttons
+const btn2 = document.querySelector('#btn2');
+btn2.onclick = () => {
+  console.log('btn2 clicked');
+};
+
+const btn3 = document.querySelector('#btn3');
+btn3.addEventListener('click', () => {
+  console.log('btn3 clicked');
+});
+
+//keys
+const input = document.querySelectorAll('input')[1];
+input.addEventListener('keydown', (e) => {console.log(e.key); console.log(e.code)});
+
+//form
+const form = document.querySelector('#shelter_form');
+const cat_input = document.querySelector('#cat_name');
+const cats_list = document.querySelector('#cats_list'); 
+const para2 = document.createElement('p');
+
+cat_input.addEventListener('input' , () => {
+    para2.textContent = cat_input.value;
+    form.append(para2);
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const cat_name = cat_input.value
+    const new_li = document.createElement('li');
+    new_li.textContent = cat_input.value;
+    cats_list.appendChild(new_li);
+    cat_input.value = "";
+    para2.textContent = "";
+});
+
+// e.stopPropagation(); to stop from event bubbling
 
 
+// event delegation
+cats_list.addEventListener('click', function(e) {
+    e.target.nodeName === "LI" && e.target.remove();
+})
+
+// ping-pong score keeper
+const p1 = {
+    score : 0,
+    button : document.querySelector('#p1button'),
+    span : document.querySelector('#p1score')
+}
+const p2 = {
+    score : 0,
+    button : document.querySelector('#p2button'),
+    span : document.querySelector('#p2score')
+}
+function updateScores(player, opponent){
+    if(!is_game_over){
+        player.score +=1;
+        if (player.score === winning_score){
+            is_game_over = true;
+            player.span.classList.add('has-text-success');
+            opponent.span.classList.add('has-text-danger');
+            player.button.disabled = true;
+            opponent.button.disabled = true;
+        }
+        player.span.textContent = player.score;
+    }
+}
+let is_game_over = false;
+let winning_score = 3;
+const reset_button = document.querySelector('#reset_button');
+const winning_score_select = document.querySelector('#winning_score')
+
+p1.button.addEventListener('click', () => {
+    updateScores(p1, p2);
+});
+p2.button.addEventListener('click', () => {
+    updateScores(p2, p1)
+});
+reset_button.addEventListener('click', reset)
+winning_score_select.addEventListener('change', function () {
+    winning_score = parseInt(this.value);
+    reset();
+})
+function reset() {
+    is_game_over = false;
+    for (let p of [p1, p2]){
+        p.score = 0;
+        p.span.textContent = p1.score;
+        p.span.classList.remove('has-text-success', 'has-text-danger');
+        p.button.disabled = false;
+    }
+}
+
+// practice qs 
+const toggle_button = document.querySelector('#toggle_button');
+const toggle_box = document.querySelector('#box');
+toggle_button.addEventListener('click', () => {
+    toggle_box.classList.toggle('has-background-info')
+})
---
+++ Task-2/Scripts/app.js
@@ -0,0 +1,302 @@
+// google button 
+const google_button = document.querySelector('.continue-google-button');
+const google_icon = document.createElement('i');
+google_icon.classList.add('fa-brands', 'fa-google', 'me-1');
+const google_img = document.querySelector('.google-img');
+
+google_button.addEventListener('mouseover', function () {
+    google_img.remove();
+    google_button.prepend(google_icon);
+})
+google_button.addEventListener('mouseleave', function () {
+    google_icon.remove();
+    google_button.prepend(google_img);
+})
+
+// show/hide password
+
+const eye_icon = document.querySelectorAll('.eye-icon');
+for (let eye of eye_icon) {
+    const password_input = eye.parentElement.parentElement.previousElementSibling;
+    eye.addEventListener('click', function () {
+        eye.classList.toggle('fa-eye');
+        eye.classList.toggle('fa-eye-slash');
+        if (password_input.type == "password") {
+            password_input.type = "text";
+        }
+        else {
+            password_input.type = "password";
+        }
+    });
+}
+
+// required fields 
+const password = {
+    input: document.querySelector('#password'),
+    name: "Password",
+    get value() { return this.input.value.trim(); },
+    isEmpty() { return !this.value; },
+    filled: false
+}
+const email = {
+    input: document.querySelector('#email'),
+    name: "Email",
+    get value() { return this.input.value.trim(); },
+    isEmpty() { return !this.value; },
+    filled: false
+}
+const confirm_password = {
+    input: document.querySelector('#confirm_password'),
+    name: "Confirm Password",
+    get value() { return this.input ? this.input.value.trim() : null },
+    isEmpty() { return !this.value; },
+    filled: false
+}
+const signup_form = document.querySelector('#signup_form');
+const login_form = document.querySelector('#login_form');
+const req_msg = document.querySelector('#required_msg');
+const submit_button = document.querySelector('#submit_button');
+
+const p_checklist = document.querySelector('#p_checklist');
+const p_checklist_items = [
+    { name: "length_check", message: "Atleast 8 characters", check() { return password.value.length >= 8 } },
+    { name: "upper_check", message: "Atleast 1 uppercase letter", check() { return password.value !== password.value.toLowerCase() ? true : false } },
+    { name: "number_check", message: "Atleast 1 number", check() { return [...password.value].some(char => (char !== ' ') && (!isNaN(char))); } }
+];
+if (signup_form) {
+    signup_form.addEventListener('submit', signupCheck);
+}
+if (login_form) {
+    login_form.addEventListener('submit', loginCheck);
+}
+function showError(field) {
+    field.input.classList.add('border-danger');
+    field.input.previousElementSibling.classList.add('border-danger');
+    if (field.input.nextElementSibling !== null) {
+        field.input.nextElementSibling.classList.add('border-danger');
+    }
+}
+function removeError(field) {
+    field.input.classList.remove('border-danger');
+    field.input.previousElementSibling.classList.remove('border-danger');
+    if (field.input.nextElementSibling !== null) {
+        field.input.nextElementSibling.classList.remove('border-danger');
+    }
+    req_msg.textContent = "";
+}
+
+function signupCheck(e) {
+    e.preventDefault();
+    let empty_fields = [];
+    if (password.isEmpty() || !passChecklist(p_checklist_items)) {
+        empty_fields.push(password);
+    }
+    if (email.isEmpty()) {
+        empty_fields.push(email);
+    }
+    if (confirm_password.input && (confirm_password.isEmpty() || !passChecklist(cp_checklist_items))) {
+        empty_fields.push(confirm_password);
+    }
+    if (empty_fields.length > 0) {
+
+
+        empty_fields.forEach(field => {
+            showError(field);
+        });
+
+
+        let message = empty_fields
+            .map(field => field.name)
+            .join(", ");
+
+
+        const error_icon = document.createElement('i');
+        error_icon.classList.add('fa-solid', 'fa-exclamation', 'me-2');
+
+
+        req_msg.textContent = "";
+
+        req_msg.append(
+            error_icon,
+            `${message} ${empty_fields.length > 1 ? "are" : "is"} required.`
+        );
+
+
+        req_msg.classList.remove('text-success');
+        req_msg.classList.add('text-danger');
+        submit_button.disabled = true;
+
+        return;
+
+
+    }
+    req_msg.textContent = "";
+    console.log('submitted');
+}
+
+function loginCheck(e) {
+
+    e.preventDefault();
+
+    let empty_fields = [];
+
+    if (password.isEmpty()) {
+        empty_fields.push(password);
+    }
+
+    if (email.isEmpty()) {
+        empty_fields.push(email);
+    }
+
+
+    if (empty_fields.length > 0) {
+
+        empty_fields.forEach(field => {
+            showError(field);
+        });
+
+        let message = empty_fields
+            .map(field => field.name)
+            .join(", ");
+
+
+        req_msg.textContent =
+            `${message} ${empty_fields.length > 1 ? "are" : "is"} required.`;
+        req_msg.classList.add('text-danger');
+        submit_button.disabled = true;
+        return;
+    }
+    req_msg.textContent = "";
+    console.log("Login submitted");
+}
+
+email.input.addEventListener('input', () => {
+    if (!email.isEmpty()) {
+        removeError(email);
+        email.filled = true;
+        checkAllFieldsFilled();
+
+    }
+    else {
+        email.filled = false;
+        showError(email);
+    }
+});
+if (confirm_password.input) {
+
+    confirm_password.input.addEventListener('input', () => {
+        if (!confirm_password.isEmpty() && passChecklist(cp_checklist_items)) {
+            removeError(confirm_password);
+            confirm_password.filled = true;
+            checkAllFieldsFilled();
+        }
+        else {
+            confirm_password.filled = false;
+            showError(confirm_password);
+        }
+    });
+}
+password.input.addEventListener('input', () => {
+    if (!password.isEmpty() && passChecklist(p_checklist_items)) {
+        removeError(password);
+        password.filled = true;
+        checkAllFieldsFilled();
+    }
+    else {
+        password.filled = false;
+        showError(password)
+    }
+});
+
+function checkAllFieldsFilled() {
+
+    if (email.filled && password.filled && (!confirm_password.input || confirm_password.filled)) {
+        submit_button.disabled = false;
+        req_msg.textContent = "";
+    }
+}
+
+// password requirements
+if (confirm_password.input) {
+    password.input.addEventListener('change', function () {
+        inputChecklist(p_checklist, p_checklist_items);
+    })
+}
+function passChecklist(checklist) {
+    return checklist.every(item => item.check());
+}
+
+function inputChecklist(checklist, checklist_items) {
+    checklist.innerHTML = "";
+    for (let item of checklist_items) {
+        if (item.check()) { continue };
+        const l_item = document.createElement('li');
+        l_item.textContent = item.message;
+        checklist.appendChild(l_item);
+    }
+}
+const cp_checklist = document.querySelector('#cp_checklist');
+const cp_checklist_items = [{ name: "password_match", message: "Passwords donot match", check() { return password.value === confirm_password.value } }];
+
+if (confirm_password.input) {
+    confirm_password.input.addEventListener('change', function () {
+        inputChecklist(cp_checklist, cp_checklist_items);
+    });
+}
+
+// Avatar update
+
+const profile_picture = document.querySelector('.profile-picture');
+const profile_input = document.querySelector('#profile_image');
+const extra_div = document.querySelector('.extra-div');
+
+if (extra_div) {
+
+    const profile_icon = extra_div.querySelector('i');
+    const profile_text = extra_div.querySelector('.picture-text');
+    profile_hover();
+
+    profile_input.addEventListener('change', function () {
+        const file = profile_input.files[0];
+        if (file && file.type.startsWith("image/")) {
+            const image_url = URL.createObjectURL(file);
+            profile_picture.style.backgroundImage = `url(${image_url})`;
+
+            profile_picture.classList.add('profile-image');
+            profile_icon.style.display = "none";
+            profile_text.style.display = "none";
+        }
+    })
+    function profile_hover() {
+        profile_picture.addEventListener('mouseenter', function () {
+            if (profile_picture.classList.contains('profile-image')) {
+                profile_icon.style.display = "block";
+                profile_text.textContent = "Remove Avatar"
+                profile_text.classList.add('px-2')
+                profile_text.style.display = "block";
+                profile_picture.classList.remove('hover-class');
+            }
+        })
+        profile_picture.addEventListener('mouseleave', function () {
+            if (profile_picture.classList.contains('profile-image')) {
+                profile_icon.style.display = "none";
+                profile_text.style.display = "none";
+            }
+        });
+    }
+
+    profile_picture.addEventListener('click', function (e) {
+        if (profile_picture.classList.contains('profile-image')) {
+            e.preventDefault();
+            profile_picture.style.backgroundImage = "none";
+            profile_picture.classList.remove('profile-image');
+            profile_text.textContent = "Add your avatar";
+            profile_text.classList.remove('px-2');
+            profile_text.style.display = "block";
+            profile_icon.style.display = "block";
+            profile_input.value = "";
+        }
+    })
+
+
+}
